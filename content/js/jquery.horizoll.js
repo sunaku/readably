c0a7ab96e2848dbf957dba6848f8fadf0e607bbe
Suraj N. Kurapati <sunaku@gmail.com>
2013-10-05 20:03:25 -0700

    add explicit ISC license to the horizoll plugin

--- content/js/jquery.horizoll.js
+++ content/js/jquery.horizoll.js
@@ -1,4 +1,7 @@
-/*
+/*****************************************************************************
+
+  jquery.horizoll.js
+  ==================
 
   Keyboard and mouse enabled horizontal screen
   scrolling plugin, with automatic realignment.
@@ -16,7 +19,26 @@
   Optional: jquery-mousewheel 3.1.3 or newer
   https://github.com/brandonaaron/jquery-mousewheel
 
-*/
+******************************************************************************
+
+  (the ISC license)
+
+  Copyright 2013 Suraj N. Kurapati <sunaku@gmail.com>
+
+  Permission to use, copy, modify, and/or distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*****************************************************************************/
+
 $(function() {
   var $window   = $(window),
       $document = $(document),
